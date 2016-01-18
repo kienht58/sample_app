e461656b9077c553716e3b25cf5f9400458d4070
kienht58 <kien.isp15@gmail.com>
2016-01-18 23:20:14 +0700

    Make a basic User model

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -5,11 +5,16 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   #   assert true
   # end
   test "layout links" do
-  	get root_path
-  	assert_template 'static_pages/home'
-  	assert_select "a[href=?]", root_path, count: 2
-  	assert_select "a[href=?]", help_path
+    get root_path
+    assert_template 'static_pages/home'
+    	
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+
+    get signup_path
+    assert_template 'users/new'
+    assert_select "h1", "Sign up"
   end
 end
